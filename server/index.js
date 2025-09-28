2ba6c654952bfae1e2b5d730c25099c263933c04
Ashutosh raj <ashu7890raj@gmail.com>
2025-09-28 16:13:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
